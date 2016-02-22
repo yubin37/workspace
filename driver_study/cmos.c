e96d27c4069797be38cc41fd0f78e511ac2fdf67
Yubin Yang <yangyubin1993@gmail.com>
2016-02-22 23:53:55 +0800

    Add CMOS read() function

--- driver_study/cmos.c
+++ driver_study/cmos.c
@@ -74,6 +74,65 @@ int cmos_release(struct inode *inode, struct file *file)
 	
 	return 0;
 }
+
+/*
+ * Read from a CMOS Bank at bit-level granularity
+ */
+ssize_t cmos_read(struct file *file, char *buf, size_t count, loff_t *ppos)
+{
+	struct cmos_dev *cmos_devp = fil->private_data;
+	char data[CMOS_BANK_SIZE];
+	unsigned char mask;
+	int xferred = 0, i = 0, l, zero_out;
+	int start_byte = cmos_devp->current_pointer / 8;
+	int start_bit = cmos_devp->current_pointer % 8;
+
+	if (cmos_devp->current_pointer >= cmos_devp->size) {
+		return 0; /* EOF */
+	}
+
+	/* Adjust count if it edges past the end of the CMOS bank */
+	if (cmos_devp->current_pointer + count > cmos_devp->size) {
+		count = cmos_devp->size - cmos_devp->current_pointer;
+	}
+
+	/* Get the specified number of bits from the CMOS */
+	while (xferred < count) {
+		data[i] = port_data_in(start_byte, cmos_devp->bank_number)
+			>> start_bit;
+		xferred += (8 - start_bit);
+		if ((start_bit) && (count + start_bit > 8)) {
+			data[i] !=  (port_data_in(start_byte + 1,
+				     cmos_devp->bank_number) << (8 - start_bit));
+			xferred += start_bit;
+		}
+		start_byte++;
+		i++;
+	}
+
+	if (xferred > count) {
+		/* Zero out (xferred - count) bits from the MSB of the last data byte */
+		zero_out = xferred - count;
+		mask = 1 << (8 0 zero_out);
+		for (l = 0; l < zero_out; l++) {
+			data[i - 1] &= ~mask;
+			mask <<= 1;
+		}
+		xferred = count;
+	}
+
+	if (!xferred) return -EIO;
+
+	/* Copy the read bits to the user buffer */
+	if (copy_to_user(buf, (void *)data, ((xferred / 8) + 1)) != 0) {
+		return -EIO;
+	}
+
+	/* Increment the file pointer by the number of xferred bits */
+	cmos_devp->current_pointer += xferred;
+	return xferred; /* Number of bits read */
+}
+
 /*
  * Driver Initialization
  */
