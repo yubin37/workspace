be4df512de9ec17bc16146b62d722ad15ebd4605
Yubin Yang <yangyubin1993@gmail.com>
2016-02-22 23:29:13 +0800

    Add CMOS open() and release() function

--- driver_study/cmos.c
+++ driver_study/cmos.c
@@ -43,6 +43,38 @@ unsigned char dataports[NUM_CMOS_BANKS] = {CMOS_BANK0_DATA_PORT,
 	                                   CMOS_BANK1_DATA_PORT,};
 
 /*
+ * Open CMOS bank
+ */
+int cmos_open(struct inode *inode, struct file *file)
+{
+	struct cmos_dev *cmos_devp;
+
+	/* Get the per-device structure that contains this cdev */
+	cmos_devp = container_of(inode->i_cdev, struct cmos_dev, cdev);
+
+	/* Easy access to cmos_devp from rest of the entry points */
+	file->private_data = cmos_devp;
+
+	/* Initialize some fields */
+	cmos_devp->size = CMOS_BANK_SIZE;
+	cmos_devp->current_pointer = 0;
+
+	return 0;
+}
+
+/*
+ * Release CMOS bank
+ */
+int cmos_release(struct inode *inode, struct file *file)
+{
+	struct cmos_dev *cmos_devp = file->private_data;
+
+	/* Reset file pointer */
+	cmos_devp->current_pointer = 0;
+	
+	return 0;
+}
+/*
  * Driver Initialization
  */
 int __init cmos_init(void)
